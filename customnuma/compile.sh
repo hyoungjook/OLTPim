72e3980205d3d6618d6c2aa825fb4c52c31cd192
Hyoungjoo Kim <hyoungjoo@cmu.edu>
2025-04-17 01:12:08 +0200

    remove custom numa

--- customnuma/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# After compiling, use LD_PRELOAD=/mosaicdb/customnuma/libnuma.so
-# to inject custom numa API to the program.
-
-g++ -std=c++17 -shared -fPIC -O3 -g -o libnuma.so numa.cpp -ldl
